634ba119069dbebad2d6f9d81390226df5d44fdf
Casey Carroll <caseydean8@gmail.com>
2019-11-29 11:39:09 -0500

    Added box around name

--- re-basic-portfolio/bootcamp-portfolio/src/Components/Header.js
+++ re-basic-portfolio/bootcamp-portfolio/src/Components/Header.js
@@ -5,7 +5,9 @@ function Header() {
   return (
     <header style={headerStyle}>
       <div style={headerContainer}>
-        <h1>Casey Carroll</h1>
+        <div style={casey}>
+          <h1>Casey Carroll</h1>
+        </div>
         <div style={linkStyle}>
           <Link to="/">About</Link> | <Link to="/portfolio">Portfolio</Link> |{" "}
           <Link to="/contact">Contact</Link>
@@ -21,12 +23,17 @@ const headerStyle = {
 
 const headerContainer = {
   width: "960px",
-  margin: '0 auto',
-  padding: "10px",
+  margin: "0 auto",
   position: "relative",
-  textAlign: "left",
-  color: "#2C3E50",
+  color: "#2C3E50"
 };
+
+const casey = {
+  background: '#2C3E50',
+  width: '200px',
+  color: '#ECF0F1',
+  padding: '1px 20px',
+}
 
 const linkStyle = {
   position: "absolute",
