06a856daa2ae36b8a69ab3992e5980107b2c5842
Casey Carroll <caseydean8@gmail.com>
2019-11-30 10:30:19 -0500

    profile photo added to about page

--- re-basic-portfolio/bootcamp-portfolio/src/App.js
+++ re-basic-portfolio/bootcamp-portfolio/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import About from "./Components/About";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Router>
       <div className="App">
         <Header />
+        <About />
       </div>
       <Footer />
     </Router>
---
+++ re-basic-portfolio/bootcamp-portfolio/src/Components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import profilePic from '../Images/profile-photo.png'
+
+function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <img src={profilePic}></img>
+    </div>
+  )
+}
+
+export default About
