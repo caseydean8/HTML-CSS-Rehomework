0c25028096c837e9b240daf77fab7af9d5af800f
Casey Carroll <caseydean8@gmail.com>
2019-12-13 11:30:35 -0500

    Centered photos at all sizes on portfolio page

--- re-basic-portfolio/bootcamp-portfolio/src/Components/Portfolio.js
+++ re-basic-portfolio/bootcamp-portfolio/src/Components/Portfolio.js
@@ -7,37 +7,29 @@ import concertPic from "../Images/concert.jpg";
 
 function Portfolio() {
   return (
-    <div className="content" id="portfolio">
-      <h1>Portfolio</h1>
-      <div className="image">
-        <img src={subPic} alt="submarine"></img>
-        <div className="caption">
-          Submarine
+    <div className="content">
+      <div className="portfolio">
+        <h1 style={{marginRight: "10px", marginLeft: "10px"}}>Portfolio</h1>
+        <div className="image">
+          <img src={subPic} alt="submarine"></img>
+          <div className="caption">Submarine</div>
         </div>
-      </div>
-      <div className="image">
-        <img src={ahoyPic} alt=""></img>
-        <div className="caption">
-          Ahoy
+        <div className="image">
+          <img src={ahoyPic} alt=""></img>
+          <div className="caption">Ahoy</div>
         </div>
-      </div>
-      <div className="image">
-        <img src={hippyPic} alt=""></img>
-        <div className="caption">
-          Crystal Vibes
+        <div className="image">
+          <img src={hippyPic} alt=""></img>
+          <div className="caption">Crystal Vibes</div>
         </div>
-      </div>
 
-      <div className="image">
-        <img src={friendPic} alt=""></img>
-        <div className="caption">
-         Friendsational
+        <div className="image">
+          <img src={friendPic} alt=""></img>
+          <div className="caption">Friendsational</div>
         </div>
-      </div>
-      <div className="image">
-        <img src={concertPic} alt=""></img>
-        <div className="caption">
-          Mudio
+        <div className="image">
+          <img src={concertPic} alt=""></img>
+          <div className="caption">Mudio</div>
         </div>
       </div>
     </div>
