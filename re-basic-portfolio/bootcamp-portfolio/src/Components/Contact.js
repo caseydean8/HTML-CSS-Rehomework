9c63025cd8fbef9eb1d3f3c2a60c62cf181b00ba
Casey Carroll <caseydean8@gmail.com>
2019-12-13 19:58:49 -0500

    Added padding for smallest size screens
    Added divs inside .content div on about and content page.

--- re-basic-portfolio/bootcamp-portfolio/src/Components/Contact.js
+++ re-basic-portfolio/bootcamp-portfolio/src/Components/Contact.js
@@ -3,25 +3,38 @@ import React from "react";
 function Contact() {
   return (
     <div className="content">
-      <h1>Contact</h1>
-      {/* <form style={formStyle}> */}
-      <form>
-        <ul>
-          <li>
-            <label>Name</label>
-            <input type="text" id="name" name="name" placeholder="Yo Mama"></input>
-          </li>
-          <li>
-            <label>Email</label>
-            <input type="text" id="email" name="email" placeholder="whatever@dude.com"></input>
-          </li>
-          <li>
-            <label>Message</label>
-            <input type="text" id="message" name="message"></input>
-          </li>
-        </ul>
-      </form>
-      <button type="submit" id="button">Submit</button>
+      <div className="contact">
+        <h1>Contact</h1>
+        <form>
+          <ul>
+            <li>
+              <label>Name</label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Yo Mama"
+              ></input>
+            </li>
+            <li>
+              <label>Email</label>
+              <input
+                type="text"
+                id="email"
+                name="email"
+                placeholder="whatever@dude.com"
+              ></input>
+            </li>
+            <li>
+              <label>Message</label>
+              <input type="text" id="message" name="message"></input>
+            </li>
+          </ul>
+        </form>
+        <button type="submit" id="button">
+          Submit
+        </button>
+      </div>
     </div>
   );
 }
